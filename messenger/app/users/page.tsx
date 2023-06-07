7ddefa952fdd9306df76e15cdbd530fa48d8ddff
osamaahmed0042 <osamaahmedkhan0042@gmail.com>
2023-06-07 19:19:51 +0500

    Peoples created

--- messenger/app/users/page.tsx
+++ messenger/app/users/page.tsx
@@ -1,15 +1,17 @@
+import EmptyState from "../components/EmptyState"
 
-'use client'
 
-import { signOut } from "next-auth/react"
+// 'use client'
+
+// import { signOut } from "next-auth/react"
 export default function Users() {
-    return <div>
+    return <div className="sm:hidden lg:block lg:pl-80 h-full bg-red-100">
 
-        Hello User
 
-        <button
+        <EmptyState />
+        {/* <button
             className="border border-black block bg-red-500 mx-5 px-5 py-3 rounded-md mt-5"
-            onClick={() => signOut()}>Signout</button>
+            onClick={() => signOut()}>Signout</button> */}
     </div>
 
 }
